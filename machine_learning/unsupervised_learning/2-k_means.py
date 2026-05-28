092e6ecaa31da144e077a892dc316b4565afe33a
Xeyransa <160167368+Kheyransa@users.noreply.github.com>
2026-05-28 23:36:47 +0400

    Create 2-k_means.py

---
+++ machine_learning/unsupervised_learning/2-k_means.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Defines a function that creates and fits a K-Means model using Scikit-learn
+"""
+from sklearn import cluster
+
+
+def K_Means(X, n_clusters, random_state):
+    """
+    Creates and fits a K-Means clustering model on tabular data
+
+    Parameters:
+        X (numpy.ndarray): Tabular data of shape (n_samples, n_features)
+        n_clusters (int): Number of clusters to find
+        random_state (int): Random seed for reproducibility
+
+    Returns:
+        sklearn.cluster.KMeans: Fitted K-Means instance
+    """
+    kmeans = cluster.KMeans(
+        n_clusters=n_clusters,
+        random_state=random_state
+    )
+    kmeans.fit(X)
+    return kmeans
